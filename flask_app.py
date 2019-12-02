a41ef5dc351fb5a24cf9b980e23da2a13b78c38d
chetanknmiet <chetanknmiet@gmail.com>
2019-12-03 00:20:44 +0530

    updated html files

--- flask_app.py
+++ flask_app.py
@@ -6,14 +6,23 @@ import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
 import os
-from flask import Flask, render_template, url_for, request, Markup
+from flask import Flask, render_template, url_for, request, Markup, jsonify
 from flask_script import Manager
 
 app = Flask(__name__)
+df = pd.read_csv("./data/bank-additional/bank-additional-full.csv", sep=";")
 
 @app.route('/')
 def index():
     return render_template('template.html')
+
+@app.route('/_get_observation')
+def get_observation():
+    return jsonify(result = df.shape[0])
+
+@app.route('/_get_subscriber_count')
+def get_subscriber_count():
+    return jsonify(result = df[df.y=="yes"].shape[0])
 
 @app.route('/get_profile_report')
 def get_profile_report():
